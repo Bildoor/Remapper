070887e5a8fcb148b4bb4bc959fd75b53932a1fa
Horseless headless horseman <hetluncharn@hotmail.com>
2024-10-19 03:05:15 +0200

    Modified build script to feel a bit smoother upon usage

--- build.cmd
+++ build.cmd
@@ -1,6 +1,10 @@
-::Remove all files in dist directory first
+@Echo off
+Echo Removing all files in dist directory
 cd dist
 for %%p in (*) do del %%p
 cd ../
-::Compile program
+Echo Directory is now clean
+Echo Compiling program...
 "C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in ./remapper.ahk /out ./dist/remapper.exe /icon ./assets/remapper.ico /base "C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe"
+Echo Program compiled successfully!
+PAUSE
